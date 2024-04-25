84b057f92d5f02adc67fe8a4f6cb512f5369a173
cricci3 <cricci3@studenti.uninsubria.it>
2024-04-25 12:57:34 +0200

    home, navbar, login, signin, logout

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from website.views import views
+from website.auth import  auth
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bam adebayo'  # secret key for the app, no matter
+
+app.register_blueprint(views, url_prefix='/views')
+app.register_blueprint(auth, url_prefix='/auth')
 
 
 @app.route('/')
@@ -9,4 +15,5 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)  # debug=True means that every time we change the code, the server get a refresh,
+    # so we don't have to rerun manually

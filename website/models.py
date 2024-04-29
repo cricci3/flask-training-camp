e953c815dd12966c1d2da2128c9420f1f0522bc8
cricci3 <cricci3@studenti.uninsubria.it>
2024-04-29 16:22:20 +0200

    models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 # DB models
-from app import db # from app.py we import db object
+from . import db  # from this package we import db object
 from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())  # Automaticcaly get the current time and TZ
+    user_id = db.Column(db.Integer, db.ForeingKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    first_name = db.Column(db.String(150))
+    password = db.Column(db.String(150))
+    notes = db.relationship('Note')
